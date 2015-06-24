e1c6026bfbcde3e248ca07c5f329a6abb108096d
Falk Hoppe <falkhoppe81@gmail.com>
2015-06-24 20:22:09 +0200

    added optional field dsl

--- demo.js
+++ demo.js
@@ -7,6 +7,9 @@
   assert('.awesome-component').toHave( function(expect) {
       expect.to.have.attr("data-awesomeness");
       expect.to.containChild('.awesome-component-footer');
+
+      expect.optional.classes('data-awesome-default', 'data-awesome-danger', 'data-awesome-warn');
+      expect.optional.attributes('data-awesome');
   });
 
   assert('.awesome-component-action').toHave( function(expect) {
