c3b6c074342a57af57474a4a836708e457062d74
Falk Hoppe <falkhoppe81@gmail.com>
2015-06-29 17:25:06 +0200

    setup test frame work with karma, qunit and phantomjs

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/*jslint node: true, white: true, nomen: true */
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    karma = require('karma').server;
+
+gulp.task('test', function(done) {
+  "use strict";
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
+});
---
+++ tests/api-test.js
@@ -0,0 +1,12 @@
+/*jslint browser: true, devel: true, todo: true, white: true, indent: 2, nomen: true, regexp: true */
+
+/*global  QUnit */
+
+(function(){
+  "use strict";
+  QUnit.module("API");
+
+  QUnit.test("test if true is true", function (assert) {
+    assert.strictEqual(true, true, "true should be true");
+  });
+}());
