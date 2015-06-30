d71b80ce680dbee7ba38c7647dc7d7c7f1a4da29
Falk Hoppe <falkhoppe81@gmail.com>
2015-06-30 07:50:40 +0200

    added first api tests

--- tests/api-test.js
+++ tests/api-test.js
@@ -1,12 +1,17 @@
 /*jslint browser: true, devel: true, todo: true, white: true, indent: 2, nomen: true, regexp: true */
 
-/*global  QUnit */
+/*global  QUnit, component */
 
 (function(){
   "use strict";
   QUnit.module("API");
 
-  QUnit.test("test if true is true", function (assert) {
-    assert.strictEqual(true, true, "true should be true");
+  QUnit.test("test if component assertions are available", function(assert) {
+    assert.strictEqual((typeof component), "function", "should provide a function to encapsulate components");
+  });
+  //
+  QUnit.test("test if component assertions are available", function(assert) {
+    var current = component('.fubar');
+    assert.strictEqual(typeof(current.assert), "function", "should have method to configure assertions per component");
   });
 }());
