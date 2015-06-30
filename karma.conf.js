881513cd484ce198812caa6a568a370370b24a3c
Falk Hoppe <falkhoppe81@gmail.com>
2015-06-30 09:09:48 +0200

    added jquery for testing

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'node_modules/jquery/dist/jquery.js',
       'node_modules/expect-dom/vendor/expect.js',
       'node_modules/expect-dom/expect-dom.js',
       'structure-assertions.js',
